fac0bde99d69c50e6d2e2457f10d73d7b3e1dd6c
Mohit Sharma <130201481+Mohitsholey04@users.noreply.github.com>
2023-12-13 19:51:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className="container">
+          <h1 className="todo_head">To-Do List</h1>
+          <div className="todo_top">
+            <input
+              type="text"
+              value={inputValue}
+              className="form-control"
+              onChange={handleInputChange}
+              placeholder="Enter a new todo"
+            />
+            <button
+              type="button"
+              className="btn"
+              onClick={handleAddTodo}
+            >
+              Add
+            </button>
+          </div>
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>
+                {todo}
+                <button
+                  type="button"
+                  className="btn btn-outline-danger"
+                  onClick={() => handleDeleteTodo(index)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
